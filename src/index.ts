e9fe6674d1e4249077a9f254299a1c04503ddedf
sanket <code.sanket13@gmail.com>
2024-01-30 03:10:19 +0530

    refactor: why iota when there's enum

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 let iota_counter = 0;
 
+/* 
 // iota func
 function iota(reset: boolean = false) {
   if (reset) {
@@ -14,7 +15,16 @@ function iota(reset: boolean = false) {
 const OP_PUSH: number = iota(true); // 0
 const OP_PLUS: number = iota(); // 1
 const OP_DUMP: number = iota(); // 2
-const COUNT_OPS: number = iota(); // 3
+const COUNT_OPS: number = iota(); // 3 
+*/
+
+// instead of using iota we can use this
+enum OPS {
+  OP_PUSH,
+  OP_PLUS,
+  OP_DUMP,
+  COUNT_OPS,
+}
 
 // just the operation type
 type OP_Type = number[];
@@ -22,34 +32,34 @@ type OP_Type = number[];
 // what the operations do
 
 function push(x: number) {
-  return [OP_PUSH, x];
+  return [OPS.OP_PUSH, x];
 }
 
 function plus() {
-  return [OP_PLUS];
+  return [OPS.OP_PLUS];
 }
 
 function dump() {
-  return [OP_DUMP];
+  return [OPS.OP_DUMP];
 }
 
 function simulate_program(program: OP_Type[]) {
   let stack: number[] = [];
 
-  program.map((op) => {
-    if (COUNT_OPS === 4) {
+  program.map((op: OP_Type) => {
+    if (OPS.COUNT_OPS > 3) {
       throw new Error("Exhaustive handling of operations in simulation");
     }
 
-    if (op[0] === OP_PUSH) {
+    if (op[0] === OPS.OP_PUSH) {
       stack.push(op[1] as number);
-    } else if (op[0] === OP_PLUS) {
+    } else if (op[0] === OPS.OP_PLUS) {
       let a = stack.pop();
       let b = stack.pop();
       if (a !== undefined && b !== undefined) {
         stack.push(a + b);
       }
-    } else if (op[0] == OP_DUMP) {
+    } else if (op[0] == OPS.OP_DUMP) {
       let a = stack.pop();
       console.log(a);
     } else {
