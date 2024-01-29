b985790ece07cd19960a469e1612ef487065fbc8
sanket <code.sanket13@gmail.com>
2024-01-30 02:16:39 +0530

    add: implementing iota an absolute basic version of iota in ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// console.log("hello world");
-
 let iota_counter = 0;
 
 // iota func
@@ -15,20 +13,3 @@ const OP_PUSH = iota(true);
 const OP_PLUS = iota();
 const OP_DUMP = iota();
 const COUNT_OPS = iota();
-
-function push(x: number) {
-  return [OP_PUSH, x];
-}
-
-function plus() {
-  return [OP_PLUS];
-}
-
-function dump() {
-  return [OP_DUMP];
-}
-
-// function simulate_pro
-
-const program = [push(20), push(80), plus(), dump()];
-// console.log(program);
